01ac0090283d1ad94e00cedb85a0902cd3dd752d
Sebastian Springer <sspringer82@gmx.net>
2012-03-09 20:26:57 +0100

    Bring game logic to the server

--- src/server/models/card.js
+++ src/server/models/card.js
@@ -8,6 +8,9 @@ application.models.card = Backbone.Model.extend({
     sessionid: null,
     cbid: null,
 
+    gameId: null,
+    cardId: null,
+
     initialize: function() {
         var mysql = require('mysql');
 
@@ -35,9 +38,36 @@ application.models.card = Backbone.Model.extend({
     },
 
     turnCard: function(data) {
+
+        var gameId = '';
+        var cardId = '';
+
+        this.gameId = gameId;
+        this.cardId = cardId;
+
+        // gibt es eine umgedrehte karte?
+        var query = 'SELECT * FROM `cards` WHERE `active` = 1 AND `status` = 1 AND `game_id` = ' + gameId;
+        this.db.query(query, _.bind(this.handleTurnedCard, this));
+
+        // nein? -> emit und raus
+        // ja? -> vergleichen
+        // gleich? -> active = 0
+        // beide emitten
+        // ungleich -> status = 0
+        // beide emitten
+        // nächster spieler is dran
+
         this.socket.emit('reply', data);
         this.socket.broadcast.emit('turnCard', data.model.id);
         this.socket.emit('turnCard', data.model.id);
+    },
+
+    handleTurnedCard: function(err, result, fields) {
+        if (_.isEmpty(result)) {
+            var query = 'UPDATE'
+        } else {
+
+        }
     }
 
 });
