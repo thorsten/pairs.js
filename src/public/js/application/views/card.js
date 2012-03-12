5fc7f66cc7dd4f50abd9d78aa859afca8bbb6392
Thorsten Rinne <thorsten.rinne@mayflower.de>
2012-03-12 10:04:42 +0100

    Set width, too

--- src/public/js/application/views/card.js
+++ src/public/js/application/views/card.js
@@ -11,7 +11,8 @@ define(function() { return Backbone.View.extend({
         
         render: function() {
             var attrs = {
-                    'height': '100'
+                    'height': '100',
+                    'width' : '100'
                     };
             
             if (this.model.get('status') == 1) {
